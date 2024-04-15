71beb2a8e40232a43769bd86f08e165e9fc42944
alex <alex.huang@yale.edu>
2024-04-15 22:42:55 +0000

    added arp handling, and requests

---
+++ arp_handler.py
@@ -0,0 +1,51 @@
+from scapy.all import Packet, Ether, IP, ARP, ls
+from cpu_metadata import CPUMetadata
+
+ARP_OP_REQ   = 0x0001
+ARP_OP_REPLY = 0x0002
+
+class ArpHandler():
+    def __init__(self,sw,mac,ip):
+        self.table = dict()
+        self.packet_buffer = dict()
+        self.mac = '00:00:00:00:00:01'
+        self.ip = '10.0.0.1'
+        self.sw = sw
+        
+    
+    def is_ip_in_arp_table(self, ip):
+        return ip in self.table
+    
+    def find_mac(self,pkt):
+        ip = pkt[IP].dst
+        print(ip)
+        arp_req_pkt = Ether(dst="ff:ff:ff:ff:ff:ff", src=self.mac) / CPUMetadata() / ARP(hwlen=6, plen=4, op=ARP_OP_REQ, hwsrc=self.mac,
+            psrc=self.ip, hwdst='00:00:00:00:00:00', pdst=ip)
+        if ip not in self.packet_buffer:
+            self.packet_buffer[ip] = []
+        self.packet_buffer[ip].append(pkt)
+        pkt.show2()
+        
+        return arp_req_pkt
+    
+    def update_entry(self,ip,mac):
+        # TODO: Check for existing entries? Don't make copies or override.
+        self.table[ip] = mac
+        
+        self.sw.insertTableEntry(table_name='MyIngress.arp_table',
+                match_fields={'meta.next_hop_ip': [ip]},
+                action_name='MyIngress.set_ether',
+                action_params={'dst': mac})
+        if ip in self.packet_buffer:
+            held_packets = self.packet_buffer[ip]
+            del self.packet_buffer[ip]
+            return held_packets
+        else:
+            return []
+        
+        
+    # def get_arp_entry(self, ip):
+        
+        
+
+
--- controller.py
+++ controller.py
@@ -5,8 +5,13 @@ from async_sniff import sniff
 from cpu_metadata import CPUMetadata
 import time
 
+
+from arp_handler import ArpHandler
+
 ARP_OP_REQ   = 0x0001
 ARP_OP_REPLY = 0x0002
+
+PWOSPF_PROTOCOL = 89
 
 class MacLearningController(Thread):
     def __init__(self, sw, start_wait=0.3):
@@ -18,8 +23,12 @@ class MacLearningController(Thread):
         self.stop_event = Event()
         self.mac = sw.intfs[1].MAC()
         self.ip = sw.intfs[1].IP()
-        print(self.ip)
+        
+        self.arp_table = ArpHandler(sw,self.mac,self.ip)
 
+    def in_gateway(self,pkt):
+        return True
+    
     def addMacAddr(self, mac, port):
         # Don't re-add the mac-port mapping if we already have it:
         if mac in self.port_for_mac: return
@@ -29,17 +38,13 @@ class MacLearningController(Thread):
                 action_params={'port': port})
         self.port_for_mac[mac] = port
 
-    def handleArpReply(self, pkt):
-        self.addMacAddr(pkt[ARP].hwsrc, pkt[CPUMetadata].srcPort)
-        self.send(pkt)
+  
         
-
-    # Add
+        
     def sendArpReply(self,pkt):
         # Ethernet
         pkt[Ether].dst = pkt[Ether].src
         pkt[Ether].src = self.mac
-
         # ARP Modification
         pkt[ARP].op = ARP_OP_REPLY
         pkt[ARP].hwdst = pkt[ARP].hwsrc
@@ -48,14 +53,19 @@ class MacLearningController(Thread):
         pkt[ARP].hwsrc = self.mac
         pkt[ARP].psrc = ptemp
         
-        pkt.show2()
         self.send(pkt)
+        
     def handleArpRequest(self, pkt):
         self.addMacAddr(pkt[ARP].hwsrc, pkt[CPUMetadata].srcPort)
         self.sendArpReply(pkt)
-
+        
+    def handleArpReply(self, pkt):
+        self.addMacAddr(pkt[ARP].hwsrc, pkt[CPUMetadata].srcPort)
+    
     def handlePkt(self, pkt):
-        #pkt.show2()
+        print("RECIEVE")
+        pkt.show2()
+        print("")
         # if (pkt[Ether].type == 34525):
         #     return
         assert CPUMetadata in pkt, "Should only receive packets from switch with special header"
@@ -64,17 +74,39 @@ class MacLearningController(Thread):
         if pkt[CPUMetadata].fromCpu == 1: return
 
         if ARP in pkt:
+            held_packets = self.arp_table.update_entry(pkt[ARP].psrc,pkt[ARP].hwsrc)
+
             if pkt[ARP].op == ARP_OP_REQ:
                 self.handleArpRequest(pkt)
             elif pkt[ARP].op == ARP_OP_REPLY:
                 self.handleArpReply(pkt)
+                
+            for packet in held_packets:
+                self.send(packet)
+        
+        if IP in pkt:
+            if (self.in_gateway(pkt)):
+                if(self.arp_table.is_ip_in_arp_table(pkt[IP].dst)):
+                    # TODO: Should be handled but potentially if the ARP_Table boots a packet, that means that something else has been evicted.
+                    pass
+                else:
+                    # Buffers packets
+                    arp_req_pkt = self.arp_table.find_mac(pkt)
+                    self.send(arp_req_pkt)
+                    
+                
+    
 
     def send(self, *args, **override_kwargs):
         pkt = args[0]
+        
         assert CPUMetadata in pkt, "Controller must send packets with special header"
         pkt[CPUMetadata].fromCpu = 1
         kwargs = dict(iface=self.iface, verbose=False)
         kwargs.update(override_kwargs)
+        print("SEND")
+        pkt.show2()
+        print("")
         sendp(*args, **kwargs)
 
     def run(self):
